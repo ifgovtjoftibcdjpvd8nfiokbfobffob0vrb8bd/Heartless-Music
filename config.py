ee81e8b71d2d77a1921f0ba9b2b86c97c98b9a09
ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd <sonkarp463@gmail.com>
2025-04-27 01:51:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/xsvt6b.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/p83ocr.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/bdcf5x.jpg"
